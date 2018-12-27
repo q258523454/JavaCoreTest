dba73bfeefc8e0b57c44a8e4f6087205c414303b
zhangj <258523454@qq.com>
2018-12-27 19:43:07 +0800

    no message

--- src/main/java/swordoffer66/Solution29_2.java
+++ src/main/java/swordoffer66/Solution29_2.java
@@ -9,8 +9,36 @@ package swordoffer66;
  * @date :   2018-12-27
  */
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
+// 用最大堆保存这k个数，每次只和堆顶比，如果比堆顶小，删除堆顶，新数入堆。
 public class Solution29_2 {
-
-
+    public ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int length = input.length;
+        if (k > length || k == 0) {
+            return result;
+        }
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(k, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+        for (int i = 0; i < length; i++) {
+            if (maxHeap.size() != k) {
+                maxHeap.offer(input[i]);
+            } else if (maxHeap.peek() > input[i]) {
+                Integer temp = maxHeap.poll();
+                temp = null;
+                maxHeap.offer(input[i]);
+            }
+        }
+        for (Integer integer : maxHeap) {
+            result.add(integer);
+        }
+        return result;
+    }
 }
