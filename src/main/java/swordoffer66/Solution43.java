525ed7d453fd90facf4f4b21632ea49fd4c1f16f
zhangj <258523454@qq.com>
2019-01-09 15:23:11 +0800

    字符序列S=”abcXYZdef”,要求输出循环左移3位后的结果，即“XYZdefabc”

---
+++ src/main/java/swordoffer66/Solution43.java
@@ -0,0 +1,26 @@
+package swordoffer66;
+
+/**
+ * Created By
+ * 左旋转字符串
+ * 字符序列S=”abcXYZdef”,要求输出循环左移3位后的结果，即“XYZdefabc”
+ * @author :   zhangj
+ * @date :   2019-01-09
+ */
+
+public class Solution43 {
+    public String LeftRotateString(String str, int n) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        n = n % str.length();
+        String str1 = str.substring(0, n );
+        String str2 = str.substring(n, str.length());
+        return str2 + str1;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution43().LeftRotateString("abcdef", 6));
+    }
+}
--- src/main/java/swordoffer66/Solution47_2.java
+++ src/main/java/swordoffer66/Solution47_2.java
@@ -2,7 +2,7 @@ package swordoffer66;
 
 /**
  * Created By
- *
+ * 求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
  * @author :   zhangj
  * @date :   2019-01-09
  */
@@ -12,7 +12,6 @@ public class Solution47_2 {
     // 2.用try异常终止递归
     // 3.用构造函数 (java在创建对象的时候并不实例化,用C++)
     public int Sum_Solution(int n) {
-
         int sum = n;
         // sum && (sum += Sum_Solution(sum - 1)); // C++版本
         boolean b = (sum != 0) && ((sum += Sum_Solution(sum - 1)) != 0); // java版本
