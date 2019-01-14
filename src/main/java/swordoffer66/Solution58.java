fcd978770c9d2e1f93beca746fdf9ff9e68d1c55
zhangj <258523454@qq.com>
2019-01-14 16:16:59 +0800

    * 正则表达式匹配  * 请实现一个函数用来匹配包括'.'和'*'的正则表达式。模式中的字符'.'表示任意一个字符，而'*'表示它前面的字符可以出现任意次（包含0次）。  * 在本题中，匹配是指字符串的所有字符匹配整个模式。例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但是与"aa.a"和"ab*a"均不匹配

--- src/main/java/swordoffer66/Solution58.java
+++ src/main/java/swordoffer66/Solution58.java
@@ -10,4 +10,58 @@ package swordoffer66;
  * @date :   2019-01-14
  */
 public class Solution58 {
+
+    public class TreeLinkNode {
+        int val;
+        TreeLinkNode left = null;
+        TreeLinkNode right = null;
+        TreeLinkNode next = null;
+
+        TreeLinkNode(int val) {
+            this.val = val;
+        }
+    }
+
+
+    public TreeLinkNode midSearch(TreeLinkNode root) {
+        return null;
+    }
+
+    public TreeLinkNode GetNext(TreeLinkNode pNode) {
+
+        if (pNode.right == null) {
+            TreeLinkNode father = pNode.next;
+            if (father == null) {
+                return null;
+            }
+            if (father.left == pNode) {
+                return father;
+            }
+
+            if (father.right == pNode) {
+                TreeLinkNode p = father;
+                TreeLinkNode q = p.next;
+                while (q != null && q.right == p) {
+                    p = q;
+                    q = q.next;
+                }
+                if (q == null) {
+                    return null;
+                }
+                if (q.left == p) {
+                    return q;
+                }
+            }
+        }
+
+        TreeLinkNode temp = pNode.right;
+        while (temp.left != null) {
+            temp = temp.left;
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
