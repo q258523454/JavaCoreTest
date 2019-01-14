c8da23c374d2bea74541a78f953545130e70af7a
zhangj <258523454@qq.com>
2019-01-14 16:58:08 +0800

    * 对称的二叉树  * 请实现一个函数，用来判断一颗二叉树是不是对称的。注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。

---
+++ src/main/java/swordoffer66/Solution59.java
@@ -0,0 +1,34 @@
+package swordoffer66;
+
+/**
+ * Created By
+ *
+ * 对称的二叉树
+ * 请实现一个函数，用来判断一颗二叉树是不是对称的。注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
+ * @author :   zhangj
+ * @date :   2019-01-14
+ */
+
+
+public class Solution59 {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+    }
+
+    boolean isSymmetrical(TreeNode pRoot) {
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
