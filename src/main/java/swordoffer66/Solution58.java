903dc88d76a3126862393df39166cf59a85f6e6d
zhangj <258523454@qq.com>
2019-01-14 16:14:32 +0800

    * 二叉树的下一个结点  * 给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。  * 注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。

---
+++ src/main/java/swordoffer66/Solution58.java
@@ -0,0 +1,13 @@
+package swordoffer66;
+
+/**
+ * Created By
+ * 二叉树的下一个结点
+ * 给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。
+ * 注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
+ *
+ * @author :   zhangj
+ * @date :   2019-01-14
+ */
+public class Solution58 {
+}
