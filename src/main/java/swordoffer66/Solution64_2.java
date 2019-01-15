deeb0c1cdf71a8d01d8564fd7f6da71024e95a39
zhangj <258523454@qq.com>
2019-01-15 17:57:07 +0800

    * 滑动窗口的最大值  * 给定一个数组和滑动窗口的大小，找出所有滑动窗口里数值的最大值。  * 例如，如果输入数组{2,3,4,2,6,2,5,1}及滑动窗口的大小3，那么一共存在6个滑动窗口，他们的最大值分别为{4,4,6,6,6,5}；  * 针对数组{2,3,4,2,6,2,5,1}的滑动窗口有以下6个：  * {[2,3,4],2,6,2,5,1}，  * {2,[3,4,2],6,2,5,1}，  * {2,3,[4,2,6],2,5,1}，  * {2,3,4,[2,6,2],5,1}，  * {2,3,4,2,[6,2,5],1}，  * {2,3,4,2,6,[2,5,1]}。

--- src/main/java/swordoffer66/Solution64_2.java
+++ src/main/java/swordoffer66/Solution64_2.java
@@ -1,5 +1,8 @@
 package swordoffer66;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * Created By
  * <p>
@@ -10,80 +13,45 @@ package swordoffer66;
  * @author :   zhangj
  * @date :   2019-01-15
  */
-public class Solution64 {
+public class Solution64_2 {
 
+    private int count = 0;
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-    LinkNode head = null;
-    public int count = 0;
-
-    public class LinkNode {
-        Integer val = null;
-        LinkNode next = null;
-
-        LinkNode(Integer val) {
-            this.val = val;
+    // Comparator 实现大根堆
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(5, new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
         }
-    }
+    });
 
     public void Insert(Integer num) {
-        count++;
-        if (head == null) {
-            head = new LinkNode(-1);
-            head.next = new LinkNode(num);
-            return;
-        }
-
-        LinkNode p = head;
-        LinkNode q = head.next;
-        while (q != null && q.val < num) {
-            q = q.next;
-            p = p.next;
-        }
-        if (q != null) {
-            LinkNode temp = new LinkNode(num);
-            temp.next = p.next;
-            p.next = temp;
+        if (count % 2 == 0) {
+            //当数据总数为偶数时，新加入的元素，应当进入小根堆
+            //（注意不是直接进入小根堆，而是经大根堆筛选后取大根堆中最大元素进入小根堆）
+            //1.新加入的元素先入到大根堆，由大根堆筛选出堆中最大的元素
+            maxHeap.offer(num);
+            int filteredMaxNum = maxHeap.poll();
+            //2.筛选后的【大根堆中的最大元素】进入小根堆
+            minHeap.offer(filteredMaxNum);
         } else {
-            p.next = new LinkNode(num);
+            //当数据总数为奇数时，新加入的元素，应当进入大根堆
+            //（注意不是直接进入大根堆，而是经小根堆筛选后取小根堆中最大元素进入大根堆）
+            //1.新加入的元素先入到小根堆，由小根堆筛选出堆中最小的元素
+            minHeap.offer(num);
+            int filteredMinNum = minHeap.poll();
+            //2.筛选后的【小根堆中的最小元素】进入大根堆
+            maxHeap.offer(filteredMinNum);
         }
-
-
+        count++;
     }
 
     public Double GetMedian() {
-        if (count == 0) {
-            return null;
-        }
-        if (count == 1) {
-            return Double.valueOf(head.next.val);
-        }
         if (count % 2 == 0) {
-            int n = count / 2;
-            LinkNode temp = head;
-            while (n > 0) {
-                temp = temp.next;
-                n--;
-            }
-            return Double.valueOf((temp.val + temp.next.val) / 2.0);
+            return new Double((minHeap.peek() + maxHeap.peek())) / 2;
         } else {
-            int n = count / 2 + 1;
-            LinkNode temp = head;
-            while (n > 0) {
-                temp = temp.next;
-                n--;
-            }
-            return Double.valueOf(temp.val);
+            return new Double(minHeap.peek());
         }
-    }
-
-
-    public static void main(String[] args) {
-        Solution64 solution64 = new Solution64();
-        int[] input = new int[]{5, 2, 3, 4, 1, 6, 7, 0, 8};
-        for (int i = 0; i < input.length; i++) {
-            solution64.Insert(input[i]);
-            System.out.println(solution64.GetMedian());
-        }
-
     }
 }
