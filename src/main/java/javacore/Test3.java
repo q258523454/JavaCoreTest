b85d773e162d0ecf809ec80308cba791715d7acf
zhangj <258523454@qq.com>
2019-04-01 17:49:36 +0800

    no message

--- src/main/java/javacore/Test3.java
+++ src/main/java/javacore/Test3.java
@@ -7,22 +7,7 @@ package javacore;
  * @date :   2019-02-28
  */
 public class Test3 {
-    private static Test3 Test3 = new Test3();
-    public static int count1;
-    public static int count2 = 0;
-
-    private Test3() {
-        count1++;
-        count2++;
-    }
-
-    public static Test3 getInstance() {
-        return Test3;
-    }
 
     public static void main(String[] args) {
-        if (0 == 0) {
-            System.out.println(1);
-        }
     }
 }
