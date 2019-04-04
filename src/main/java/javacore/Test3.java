2666de1861a8b4a08616487d23f7c35486b33c02
zhangj <258523454@qq.com>
2019-04-04 16:26:31 +0800

    no message

--- src/main/java/javacore/Test3.java
+++ src/main/java/javacore/Test3.java
@@ -1,5 +1,10 @@
 package javacore;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * Created By
  *
@@ -7,7 +12,38 @@ package javacore;
  * @date :   2019-02-28
  */
 public class Test3 {
-
     public static void main(String[] args) {
+        HashSet<HashMap<String, String>> hashSet = new HashSet<>();
+        HashMap hashMap1 = new HashMap<String, String>();
+        HashMap hashMap2 = new HashMap<String, String>();
+        HashMap hashMap3 = new HashMap<String, String>();
+        hashMap1.put("a", "1");
+        hashMap2.put("b", "2");
+        hashMap3.put("a", "1");
+        hashSet.add(hashMap1);
+        hashSet.add(hashMap2);
+        hashSet.add(hashMap3);
+        System.out.println(hashSet.size());
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        System.out.println(list.size());
+
+        // 创建集合对象
+        HashSet<String> hs = new HashSet<String>();
+
+        // 创建并添加元素
+        hs.add("hello");
+        hs.add("world");
+        hs.add("java");
+        hs.add("world");
+
+        // 遍历集合
+        for (String s : hs) {
+            System.out.println(s);
+        }
+
     }
 }
+
